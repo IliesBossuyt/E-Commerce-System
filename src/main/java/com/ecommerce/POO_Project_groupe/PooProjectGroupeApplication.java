7dea19efd3c3df1c048edcb70d2de238ef3c0c2d
Leocollowald <leo.collowald@ynov.com>
2025-03-11 11:52:08 +0100

    Spring boot

---
+++ src/main/java/com/ecommerce/POO_Project_groupe/PooProjectGroupeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.POO_Project_groupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooProjectGroupeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooProjectGroupeApplication.class, args);
+	}
+
+}
